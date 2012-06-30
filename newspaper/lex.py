7be0fa3e2eaabca1484242bfd021f2fe729ca497
gnidan <nick@gnidan.org>
2012-06-30 15:48:39 -0400

    fixed parser, removed some tmp output from repo

--- newspaper/lex.py
+++ newspaper/lex.py
@@ -20,9 +20,29 @@ t_ignore      = ' \t'
 def t_newline(t):
   r'\n'
   t.lexer.lineno += 1
+  t.type = 'NEWLINE'
+  return t
 
 def t_error(t):
   print "Illegal character '%s'" % t.value[0]
   t.lexer.skip(1)
 
 lexer = lex.lex()
+
+data ='''Header
+=======
+
+This is a paragraph. Hello!
+
+Another paragraph. This time
+with more than one line.
+
+'''
+if __name__ == '__main__':
+  lexer.input(data)
+  while True:
+    tok = lexer.token()
+    if not tok:
+      break
+    print tok
+
--- newspaper/yacc.py
+++ newspaper/yacc.py
@@ -5,87 +5,75 @@
 # ------------------------------------------------------------
 import ply.yacc as yacc
 
-import pdb
 from lex import tokens
 
 def p_document(p):
   'document : block_list'
-  pdb.set_trace()
   p[0] = p[1]
-  print 1
 
 def p_blocklist(p):
   '''block_list : block block_list
                 | '''
-  pdb.set_trace()
   if len(p) == 3:
-    p[0] = p[2].insert(0, p[1])
+    p[2].insert(0, p[1])
+    p[0] = p[2]
   elif len(p) == 1:
     p[0] = []
-  print 2
 
 def p_block(p):
-  '''block : header NEWLINE
-           | paragraph NEWLINE'''
-  pdb.set_trace()
+  '''block : line header blank
+        | paragraph blank
+        | blank'''
   p[0] = p[1]
-  print 3
-
-def p_header(p):
-  'header : line HEADER'
-  pdb.set_trace()
-  p[0] = p[1]
-  print 5
-
-def p_paragraph(p):
-  '''paragraph : 
-               | line paragraph'''
-  pdb.set_trace()
-  if len(p) == 3:
-    p[0] = p[2].insert(0, p[1])
-  elif len(p) == 1:
-    p[0] = []
-  print 6
 
 def p_line(p):
-  '''line : NEWLINE
-          | WORD line'''
-  pdb.set_trace()
+  '''line : word_list NEWLINE'''
   p[0] = p[1]
-  if len(p) == 3:
-    if isinstance(p[2], str):
-      p[2] = []
 
-    p[0] = p[2].insert(0, p[1])
-  elif len(p) == 1:
-    p[0] = []
-  print 7
+def p_header(p):
+  '''header : HEADER NEWLINE'''
+  p[0] = p[1]
+
+def p_blank(p):
+  '''blank : NEWLINE'''
+  p[0] = p[1]
+
+def p_paragraph(p):
+  '''paragraph : line paragraph
+        | line'''
+  if len(p) == 3:
+    p[2].insert(0, p[1])
+    p[0] = p[2]
+  else:
+    p[0] = [ p[1] ]
+
+def p_wordlist(p):
+  '''word_list : word_list WORD
+        | WORD'''
+  if len(p) == 3:
+    p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [ p[1] ]
 
 def p_error(p):
   print "Syntax error in input!"
 
-import logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename="parselog.txt",
-    filemode="w",
-    format="%(filename)10s:%(lineno)4d:%(message)s")
-log = logging.getLogger()
 
-parser = yacc.yacc(debug=True,debuglog=log)
+parser = yacc.yacc()
 
-data = '''
-Header
-======
 
-This is a paragraph
+data ='''Header
+=======
 
-This is another paragraph
-with multiple lines in it.
-The quick brown fox jumps over
-the lazy dog.
+This is a paragraph. Hello!
+
+Another paragraph. This time
+with more than one line.
 
 '''
 
 result = parser.parse(data)
-print result
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+print pp.pprint(result)
